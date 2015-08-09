7321dd523506ba977920eb1fc2c9f02de087efbb
Josh Long <josh@joshlong.com>
2015-08-08 19:27:21 -0700

    rebuilding site Sat Aug  8 19:27:21 PDT 2015

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:cloud-native-java/cloud-native-java.git gh-pages
+git subtree push --prefix=public git@github.com:cloud-native-java/cloud-native-java.github.io.git gh-pages
