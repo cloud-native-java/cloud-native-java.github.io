6b15c73d1b96fcc6db933a096d526c7f99f09df6
Josh Long <josh@joshlong.com>
2017-08-28 16:44:06 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
 echo -e "deploying updates to Github.."
-
-# Build the project.
 hugo
 cp CNAME public/CNAME
 git add public/CNAME
 git commit -a -m "adding CNAME"
-
 git add -A
-
-# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
-# git pull origin source
 git push origin source
-# git subtree push  --prefix=public git@github.com:cloud-native-java/cloud-native-java.github.io.git master
-
-
-echo git push public `git subtree split --prefix public source`:master --force
+git push origin `git subtree split --prefix public source`:master --force
